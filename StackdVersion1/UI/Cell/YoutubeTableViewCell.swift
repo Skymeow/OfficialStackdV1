df21e873cab3efc22f114005bf837f7adb067ab6
Sky Xu <skyrocks88xmz@gmail.com>
2018-05-24 16:37:45 -0700

    fixed tags cell

--- StackdVersion1/UI/Cell/YoutubeTableViewCell.swift
+++ StackdVersion1/UI/Cell/YoutubeTableViewCell.swift
@@ -16,26 +16,56 @@ class YoutubeTableViewCell: UITableViewCell {
     @IBOutlet weak var duration: UILabel!
     @IBOutlet weak var sourceImg: UIImageView!
     @IBOutlet weak var sourceTitle: UILabel!
-    @IBOutlet weak var tagLabel2: UILabel!
-    @IBOutlet weak var tagLabel1: UILabel!
-    @IBOutlet weak var tagLabel3: UILabel!
-  
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     var tagsData: [Tags]?
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        let tagCell = UINib(nibName: "TagsCell", bundle: Bundle.main)
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = UICollectionViewScrollDirection.horizontal
+        layout.itemSize = CGSize(width: 45, height: 20)
+        layout.sectionInset = .zero
+        collectionView.collectionViewLayout = layout
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(tagCell, forCellWithReuseIdentifier: "TagsCell")
     }
     
     override func layoutSubviews() {
-        if let tags = self.tagsData {
-            for tag in tags{
-                tagLabel1.text = tag.content ?? ""
-                tagLabel1.adjustsFontSizeToFitWidth = true
-                tagLabel2.text = tag.content ?? ""
-                tagLabel2.adjustsFontSizeToFitWidth = true
-                tagLabel3.text = tag.content ?? ""
-                tagLabel3.adjustsFontSizeToFitWidth = true
-            }
-        }
+        
     }
     
 }
+
+extension YoutubeTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return self.tagsData?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TagsCell", for: indexPath) as! TagsCell
+        cell.backgroundColor = .lightGray
+        if let tags = self.tagsData {
+            cell.label.text = tags[indexPath.row].content ?? ""
+            cell.label.adjustsFontSizeToFitWidth = true
+        }
+        return cell
+    }
+}
+
+//extension YoutubeTableViewCell: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let width = CGFloat(25)
+//
+//        return CGSize(width: width, height: collectionView.bounds.height - 2)
+//    }
+//}
+
+
