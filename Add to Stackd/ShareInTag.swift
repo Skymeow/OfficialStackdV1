2dd9dd2cf5b92b2bd499915fbdc6a7abf110fb1f
Sky Xu <skyrocks88xmz@gmail.com>
2018-06-09 22:05:45 -0700

    added shareinpopup

---
+++ Add to Stackd/ShareInTag.swift
@@ -0,0 +1,19 @@
+//
+//  ShareInTag.swift
+//  Add to Stackd
+//
+//  Created by Sky Xu on 6/9/18.
+//  Copyright © 2018 Sky Xu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ShareInTag: UIView {
+   
+    
+    func dismiss() {
+        self.removeFromSuperview()
+    }
+    
+}
